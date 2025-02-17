b0629e21e7974722b27dd181effb848aa95d0a53
Kirk810 <adriansoto89@gmail.com>
2025-02-17 12:42:15 +0100

    cambios

--- sale_helpdesk_adriansoto/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import sale_order
-from . import helpdesk_ticket
-from . import product_product
--- sale_helpdesk_adriansoto/models/helpdesk_ticket.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, api, fields, _
-
-class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
-    
-    sale_id = fields.Many2one(
-        comodel_name='sale.order',
-        string='Sale Order'
-    )
